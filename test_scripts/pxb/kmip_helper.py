f232079bcab1ea47d9662019732d11f9471c4d90
Tomislav Plavcic <tplavcic@gmail.com>
2026-02-20 12:36:47 +0100

    Remove fortanix from default kmip config

--- test_scripts/pxb/kmip_helper.py
+++ test_scripts/pxb/kmip_helper.py
@@ -33,7 +33,7 @@ class KMIPHelper:
     # Default KMIP configurations
     DEFAULT_KMIP_CONFIGS = {
         "pykmip": "addr=127.0.0.1,image=satyapercona/kmip:latest,port=5696,name=kmip_pykmip",
-        "fortanix": "addr=216.180.120.88,port=5696,name=kmip_fortanix,setup_script=fortanix_kmip_setup.py",
+        # "fortanix": "addr=216.180.120.88,port=5696,name=kmip_fortanix,setup_script=fortanix_kmip_setup.py",
         # "hashicorp": "addr=127.0.0.1,port=5696,name=kmip_hashicorp,setup_script=hashicorp-kmip-setup.sh",
         # "ciphertrust": "addr=127.0.0.1,port=5696,name=kmip_ciphertrust,setup_script=setup_kmip_api.py",
     }
